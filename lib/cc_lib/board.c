dfdb6b7a83db8f90c3e6bfed1511b546f169bcb0
Stefan Kraus <dev@stefankraus.org>
2025-09-10 13:32:08 +0200

    chore: add SPDX License information to files
    Signed-off-by: Stefan Kraus <dev@stefankraus.org>

--- lib/cc_lib/board.c
+++ lib/cc_lib/board.c
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Alicipy <dev@stefankraus.org>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include "cc_lib/board.h"
 
 #include "cc_lib/aldrin.h"
--- subsys/cc_temperature/src/dht_sensor.c
+++ subsys/cc_temperature/src/dht_sensor.c
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Alicipy <dev@stefankraus.org>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/logging/log.h>
